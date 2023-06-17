46f2f69a99c3a974f2870b551f1d0dd47c545e46
zancas <zancas@zingolabs.org>
2023-06-17 14:35:32 -0600

    start on plotters example

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use plotters::{backend, drawing};
+
+const OUT_FILE_NAME: &'static str = "normal-dist.png";
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let root = drawing::IntoDrawingArea::into_drawing_area(backend::BitMapBackend::new(
+        OUT_FILE_NAME,
+        (1024, 768),
+    ));
+    todo!()
+}
