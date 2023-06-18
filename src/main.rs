0ec046c12dcf86ba4ae78a8258f82dfc3e199299
zancas <zancas@zingolabs.org>
2023-06-17 18:56:17 -0600

    prefer explicit trait method invocations and explicit mod paths

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use plotters::style::colors::WHITE;
-use plotters::{backend, drawing};
+use plotters::{backend, chart, coord, drawing, style};
 use rand::SeedableRng;
 use rand_distr::{Distribution, Normal};
 use rand_xorshift::XorShiftRng;
@@ -10,7 +9,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         OUT_FILE_NAME,
         (1024, 768),
     ));
-    root.fill(&WHITE)?;
+    root.fill(&style::colors::WHITE)?;
 
     let sd = 0.13;
     let random_points: Vec<(f64, f64)> = {
@@ -21,5 +20,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let y_iter = norm_dist.sample_iter(&mut y_rand);
         x_iter.zip(y_iter).take(10_000).collect()
     };
+    let areas = root.split_by_breakpoints([944], [80]);
+    let linspace = coord::combinators::IntoLinspace::step(0.0..1.0, 0.01).use_round();
+    let segmented_coord = coord::ranged1d::IntoSegmentedCoord::into_segmented(linspace);
+    let mut x_hist_ctx = chart::ChartBuilder::on(&areas[0])
+        .y_label_area_size(40)
+        .build_cartesian_2d(segmented_coord, 0..250)?;
     todo!()
 }
